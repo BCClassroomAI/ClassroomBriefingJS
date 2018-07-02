3e970a42b04f4bf45e1d40f2862ac6d912fe1d62
Will <farleywa@bc.edu>
2018-07-02 12:25:58 -0400

    restructured and node modules removed

--- ClassroomBriefing-332c9bc5-9248-4fab-a520-3154aa8a9b28(1) 2/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm ../ClassroomBriefing.zip
-
-zip -r ../ClassroomBriefing.zip . -x "*.git*"
-
-aws lambda update-function-code --function-name ClassroomBriefing --zip-file fileb://../ClassroomBriefing.zip
